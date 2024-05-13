3c90900beed91b0452377b9838d26a115a277ccc
Ikey Doherty <ikey@serpentos.com>
2024-05-13 22:12:13 +0100

    Add initial `blsctl` skeleton for drop-in cbm replacement
    This (currently do-nothing) skeleton provides a replacement
    for the old somewhat janky CLI of clr-boot-manager in order
    to facilitate a transition path.
    
    It will not be used by Serpent, as `moss` will directly consume
    the libraries.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ blsctl/src/main.rs
@@ -0,0 +1,75 @@
+// SPDX-FileCopyrightText: Copyright © 2024 Serpent OS Developers
+//
+// SPDX-License-Identifier: MPL-2.0
+
+//! Provides a CLI compatible with `clr-boot-manager` to be used as a drop-in
+//! replacement for Solus.
+
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+/// Boot Loader Specification compatible kernel/initrd/cmdline management
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    /// Override base path for all boot management operations
+    #[arg(short, long)]
+    path: Option<PathBuf>,
+
+    /// Force running in image mode (scripting integration)
+    #[arg(short, long)]
+    image: bool,
+
+    /// Do not allow updating EFI vars
+    #[arg(short, long)]
+    no_efi_update: bool,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Print version and exit
+    Version,
+
+    /// Report currently running kernel as successfully booting
+    ReportBooted,
+
+    /// Remove specified kernel from the system
+    RemoveKernel,
+
+    /// Mount the `$BOOT` directories
+    MountBoot,
+
+    /// Configure the `$BOOT` directories for next boot
+    Update,
+
+    /// Set the bootloader timeout value
+    SetTimeout { timeout: u64 },
+
+    /// Retrieve the bootloader timeout value
+    GetTimeout,
+
+    /// Set the kernel that will be used at next boot
+    SetKernel { kernel: String },
+
+    /// List kernels on `$BOOT`
+    ListKernels,
+}
+
+fn main() {
+    let res = Cli::parse();
+    match res.command {
+        Commands::Version => todo!(),
+        Commands::ReportBooted => todo!(),
+        Commands::RemoveKernel => todo!(),
+        Commands::MountBoot => todo!(),
+        Commands::Update => todo!(),
+        Commands::SetTimeout { timeout: _ } => todo!(),
+        Commands::GetTimeout => todo!(),
+        Commands::SetKernel { kernel: _ } => todo!(),
+        Commands::ListKernels => todo!(),
+    }
+}
