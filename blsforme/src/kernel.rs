648b60949dcf0ab34153ece852effe5d4656ce14
Ikey Doherty <ikey@serpentos.com>
2024-06-30 22:37:11 +0100

    kernel: Address woeful docs
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- blsforme/src/kernel.rs
+++ blsforme/src/kernel.rs
@@ -5,8 +5,14 @@
 //! Kernel abstraction
 
 use std::path::PathBuf;
+
+/// A kernel is the primary bootable element that we care about, ie
+/// the vmlinuz file. It also comes with a set of auxilliary files
+/// that are required for a fully working system, but specifically
+/// dependent on that kernel version.
 #[derive(Debug)]
 pub struct Kernel {
+    /// Matches the `uname -r` of the kernel, should be uniquely encoded by release/variant
     pub version: String,
 
     /// vmlinuz path
@@ -22,13 +28,21 @@ pub struct Kernel {
 /// Denotes the kind of auxillary file
 #[derive(Debug)]
 pub enum AuxilliaryKind {
+    /// A cmdline snippet
     Cmdline,
+
+    /// An initial ramdisk
     InitRD,
+
+    /// System.map file
     SystemMap,
+
+    /// .config file
     Config,
 }
 
-/// Any non-image file
+/// An additional file required to be shipped with the kernel,
+/// such as initrds, system maps, etc.
 #[derive(Debug)]
 pub struct AuxilliaryFile {
     pub path: PathBuf,
