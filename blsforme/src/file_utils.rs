cc11d903fed6c2c8dc0b7bbaea3bab727f9f859e
Ikey Doherty <ikey@serpentos.com>
2024-07-07 15:55:32 +0100

    blsforme/file_utils: Add file comparison utilities
    When dealing with FAT we really want to avoid writing unless it
    is absolutely necessary. To facilitate this caveat we utilise the
    parallel/mmap variant of blake3 to compare a set of paths to
    determine if they differ, and produce a reduced set of paths that
    require updating.
    
    Note, an absent source/dest is also the same as "needs update".
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- blsforme/src/file_utils.rs
+++ blsforme/src/file_utils.rs
@@ -2,13 +2,18 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-//! Case-insensitive path joining for FAT, respecting existing entries on the filesystem
-//! Note, this discards errors, so will require read permissions
+//! File utilities shared between the blsforme APIs
+
 use std::{
-    fs,
+    fs::{self, File},
+    os::unix::fs::MetadataExt,
     path::{Path, PathBuf},
 };
 
+use crate::Error;
+
+/// Case-insensitive path joining for FAT, respecting existing entries on the filesystem
+/// Note, this discards errors, so will require read permissions
 pub trait PathExt<P: AsRef<Path>> {
     fn join_insensitive(&self, path: P) -> PathBuf;
 }
@@ -29,4 +34,51 @@ impl<P: AsRef<Path>> PathExt<P> for PathBuf {
         }
         self.join(path)
     }
+}
+
+/// Compare two files with blake3 to see if they differ
+fn files_identical(hasher: &mut blake3::Hasher, a: &Path, b: &Path) -> Result<bool, Error> {
+    let fi_a = File::open(a)?;
+    let fi_b = File::open(b)?;
+    let fi_a_m = fi_a.metadata()?;
+    let fi_b_m = fi_b.metadata()?;
+    if fi_a_m.size() != fi_b_m.size() || fi_a_m.file_type() != fi_b_m.file_type() {
+        Ok(false)
+    } else {
+        hasher.update_mmap_rayon(a)?;
+        let result_a = hasher.finalize();
+        hasher.reset();
+
+        hasher.update_mmap_rayon(b)?;
+        let result_b = hasher.finalize();
+        hasher.reset();
+
+        Ok(result_a == result_b)
+    }
+}
+
+/// Find out which files in the set changed
+///
+/// Given a slice containing tuples of pathbufs, return an
+/// allocated set of cloned pathbuf tuples (pairs) known to
+/// differ.
+///
+/// The first element in the tuple should be the source path, and the
+/// right hand side should contain the destination path.
+pub fn changed_files<'a, 'b: 'a>(files: &'a [(PathBuf, PathBuf)]) -> Vec<(&'a PathBuf, &'a PathBuf)> {
+    let mut hasher = blake3::Hasher::new();
+
+    files
+        .iter()
+        .filter_map(|(source, dest)| match files_identical(&mut hasher, source, dest) {
+            Ok(same) => {
+                if same {
+                    None
+                } else {
+                    Some((source, dest))
+                }
+            }
+            Err(_) => Some((source, dest)),
+        })
+        .collect::<Vec<_>>()
 }
